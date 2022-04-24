880dcb71b08849cc2ea7df788dcb7bb666c62824
c-ostic <60513574+c-ostic@users.noreply.github.com>
2022-04-24 13:31:52 -0400

    Added a simple pause menu
    Added a simple pause menu to allow for quitting the game
    
    Also added another text file with only the gem hints

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    bool paused = false;
+
+    public GameObject panel;
+    public GameObject playerCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == false)
+            {
+                PauseGame();
+            }
+            else
+            {
+                UnPauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        paused = true;
+        playerCam.GetComponent<PlayerLook>().enabled = false;
+        Time.timeScale = 0;
+        panel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void UnPauseGame()
+    {
+        paused = false;
+        playerCam.GetComponent<PlayerLook>().enabled = true;
+        Time.timeScale = 1;
+        panel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
